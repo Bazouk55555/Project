d191ece7e8e77f6722b906819975991fbbaa6af6
Bazouk55555 <adrien_boukobza@hotmail.fr>
2016-08-14 15:03:16 +0100

    fix iostream bug

--- transform_2_jpeg.h
+++ transform_2_jpeg.h
@@ -1,5 +1,6 @@
 #ifndef TRANSFORM_2_JPEG_H
 #define TRANSFORM_2_JPEG_H
+#include<iostream>
 
 /*#include "../../../Desktop/Imebra/library/imebra/include/imebra.h"
 using namespace puntoexe;
@@ -9,6 +10,11 @@ using namespace puntoexe::imebra;*/
 int transformation(std::string source, std::string destination);
 std::string changeDicomToJpeg(std::string dicom);
 bool isDicom(std::string file);*/
+
+// Check if the file is a png image
 bool isPng(std::string file);
+
+// Check if the file is a jpg image
 bool isJpg(std::string file);
+
 #endif // TRANSFORM_2_JPEG_H
